e4dbccc4dc01bdfec3ee72766978ecdfa8d8e208
shraddha-1402 <shraddha.gupta.1402@gmail.com>
2021-08-15 22:11:40 +0530

    removed bugs, refactored

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-const form = document.querySelector("#form");
-const purPrice = document.querySelector("#purchase_price");
-const currPrice = document.querySelector("#current_price");
-const noOfStocks = document.querySelector("#no_of_stocks");
-const img = document.querySelector("#image_status");
-const output = document.querySelector("#output");
+const formElement = document.querySelector("#form");
+const purPriceElement = document.querySelector("#purchase_price");
+const currPriceElement = document.querySelector("#current_price");
+const noOfStocksElement = document.querySelector("#no_of_stocks");
+const imgElement = document.querySelector("#image_status");
+const outputElement = document.querySelector("#output");
 
 function checkProfitLoss(event) {
-  let change = Math.abs(parseInt(purPrice.value) - parseInt(currPrice.value));
-  let changePercentage = (change * 100) / parseInt(purPrice.value);
-  noOfStocks = parseInt(noOfStocks.value);
-  if (parseInt(purPrice.value) <= parseInt(currPrice.value)) {
-    // img.setAttribute("src", "./image/4003196.jpg");
-    output.innerText = `You gained ${changePercentage.toFixed(2)}%. Your total profit is ${change * noOfStocks}Rs`;
+  event.preventDefault();
+
+  let purPrice = parseInt(purPriceElement.value);
+  let currPrice = parseInt(currPriceElement.value);
+  let noOfStocks = parseInt(noOfStocksElement.value);
+  let change = Math.abs(purPrice - currPrice);
+  let changePercentage = (change * 100) / purPrice;
+  if (purPrice <= currPrice) {
+    imgElement.setAttribute("src", "./image/profit.jpg");
+    output.innerText = `You gained ${changePercentage.toFixed(2)} %. Your total profit is ${change * noOfStocks} Rs`;
   }
   else {
-    // img.setAttribute("src", "./image/output-onlinepngtools.png");
-    output.innerText = `You lost ${changePercentage.toFixed(2)}%. Your total loss is ${change * noOfStocks}Rs`;
+    imgElement.setAttribute("src", "./image/loss.png");
+    output.innerText = `You lost ${changePercentage.toFixed(2)}%. Your total loss is ${change * noOfStocks} Rs`;
   }
   output.scrollIntoView(true);
-  event.preventDefault();
 }
 
 form.addEventListener("submit", checkProfitLoss);
